3b30f520a1aaad6209e61f6ff90dda6ef4e30beb
Segami <1023557458@qq.com>
2021-09-03 20:06:28 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index'
+import './plugins/element.js'
 
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+//导入全局样式表
+import './assets/CSS/global.css'
+
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+//将axios挂载到Vue的原型对象上,让组件可以通过$http来直接调用
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    //当用户直接访问时，重定向到login界面
+    {path:'/',redirect:'/login'},
+    {path:'/login',component:Login},
+    {path:'/home',component:Home},
+
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  // next() 放行    next('/login') 强制跳转
+
+  //如果用户访问的是登录页，直接放行
+  if(to.path == "/login") return next()
+  //获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  //如果没有token，强制跳转到登录页
+  if(!tokenStr) return next('/login')
+  //如果有token则放行
+  next()
 })
 
 export default router
